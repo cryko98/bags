72c381cd9dfbfd23e3f0ae946e8c97b04fdc34dc
cryko98 <cryko98@gmail.com>
2025-12-17 07:17:42 +0200

    feat: Update contract address and Twitter link
    Replaced placeholder contract address with the actual $BAGS CA.
    Updated Twitter link to point to the official $BAGS community page.

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
-export const CA_ADDRESS = "xxxxxxxxxxxxxxxxxxxxxxxxx";
-export const TWITTER_LINK = "https://x.com/home"; 
+export const CA_ADDRESS = "5wyXSHTQZo5zh9E2Vos5uD89u3ZKrEYTZoSi52X4BAGS";
+export const TWITTER_LINK = "https://x.com/i/communities/2001099994607312951"; 
 export const BUY_LINK = "https://bags.fm"; // Updated to Bags.fm
 
 export const IMAGES = {
